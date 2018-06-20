a15ffea03889af5fc73764fcaad74fa0e2e3d613
haormj <haormj@qq.com>
2018-06-20 14:44:27 +0800

    add unit

---
+++ unit.go
@@ -0,0 +1,12 @@
+package util
+
+const (
+	// KB a unit of information used
+	KB int = 1024
+	// MB a unit of information used
+	MB int = 1024 * KB
+	// GB a unit of information used
+	GB int = 1024 * MB
+	// TB a unit of information used
+	TB int = 1024 * GB
+)
