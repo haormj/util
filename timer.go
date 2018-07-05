fedc2904978f0f543cf19f51924a8287ec43c096
haormj <haormj@qq.com>
2018-07-05 09:40:05 +0800

    add timer

---
+++ timer.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+	"context"
+	"log"
+	"time"
+)
+
+// CallbackFunc callback function
+type CallbackFunc func()
+
+// DailyTask daily execution
+func DailyTask(ctx context.Context, hour, minute, second int, loc *time.Location,
+	callback CallbackFunc) {
+	go func(ctx context.Context, hour, minute, second int, loc *time.Location,
+		callback CallbackFunc) {
+		// prevent exec callback panic
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("exec callback panic, %v\n", err)
+				// rerun DailyTask
+				go DailyTask(ctx, hour, minute, second, loc, callback)
+			}
+		}()
+		for {
+
+			now := time.Now().In(loc)
+			timing := time.Date(now.Year(), now.Month(), now.Day(), hour, minute, second, 0, loc)
+			// obtain the difference between timing and now
+			diff := timing.Sub(now)
+			if diff < 0 {
+				// add 1 day
+				diff = timing.AddDate(0, 0, 1).Sub(now)
+			}
+			select {
+			case <-time.After(diff):
+				go callback()
+			case <-ctx.Done():
+				break
+			}
+		}
+	}(ctx, hour, minute, second, loc, callback)
+}
+
+// DailyTaskCST DailyTask for CST
+func DailyTaskCST(ctx context.Context, hour, minute, second int, callback CallbackFunc) {
+	DailyTask(ctx, hour, minute, second, CST(), callback)
+}
+
+// DailyTaskUTC DailyTask for UTC
+func DailyTaskUTC(ctx context.Context, hour, minute, second int, callback CallbackFunc) {
+	DailyTask(ctx, hour, minute, second, time.UTC, callback)
+}
