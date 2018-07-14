3567573b33b3e83c925b020e8e1b79844169cf92
haormj <haormj@qq.com>
2018-07-15 01:07:01 +0800

    add random string

---
+++ random_string.go
@@ -0,0 +1,122 @@
+// Package util random string
+// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	letterBytes        = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterDigitalBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	letterIdxBits      = 6                    // 6 bits to represent a letter index
+	letterIdxMask      = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax       = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var (
+	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	src         = rand.NewSource(time.Now().UnixNano())
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func RandStringBytesRmndr(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func RandStringBytesMask(n int) string {
+	b := make([]byte, n)
+	for i := 0; i < n; {
+		if idx := int(rand.Int63() & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i++
+		}
+	}
+	return string(b)
+}
+
+func RandStringBytesMaskImpr(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func RandStringBytesMaskImprSrc(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func RandomString(source string, n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(source) {
+			b[i] = source[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func RandomStringByLetter(n int) string {
+	return RandomString(letterBytes, n)
+}
+
+func RandomStringByLetterAndDigital(n int) string {
+	return RandomString(letterDigitalBytes, n)
+}
---
+++ random_string_test.go
@@ -0,0 +1,74 @@
+package util
+
+import (
+	"log"
+	"testing"
+)
+
+func TestRandStringRunes(t *testing.T) {
+	log.Println(RandStringRunes(10))
+}
+
+func BenchmarkRandStringRunes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringRunes(60)
+	}
+}
+
+func TestRandStringBytes(t *testing.T) {
+	log.Println(RandStringBytes(10))
+}
+
+func BenchmarkRandStringBytes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytes(60)
+	}
+}
+
+func TestRandStringBytesRmndr(t *testing.T) {
+	log.Println(RandStringBytesRmndr(10))
+}
+
+func BenchmarkRandStringBytesRmndr(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytesRmndr(60)
+	}
+}
+
+func TestRandStringBytesMask(t *testing.T) {
+	log.Println(RandStringBytesMask(10))
+}
+
+func BenchmarkRandStringBytesMask(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytesMask(60)
+	}
+}
+
+func TestRandStringBytesMaskImpr(t *testing.T) {
+	log.Println(RandStringBytesMaskImpr(10))
+}
+
+func BenchmarkRandStringBytesMaskImpr(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytesMaskImpr(60)
+	}
+}
+
+func TestRandStringBytesMaskImprSrc(t *testing.T) {
+	log.Println(RandStringBytesMaskImprSrc(10))
+}
+
+func BenchmarkRandStringBytesMaskImprSrc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytesMaskImpr(60)
+	}
+}
+
+func TestRandomString(t *testing.T) {
+	log.Println(RandomString("ab", 10))
+}
+
+func TestRandomStringByLetterAndDigital(t *testing.T) {
+	log.Println(RandomStringByLetterAndDigital(10))
+}
