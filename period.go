45601824042da023957e4f54f8774e48a0ee49ee
haormj <haormj@gmail.com>
2020-04-05 00:37:09 +0800

    util: add period impl
    period is a length of time and provide a set of method to operate period

---
+++ period.go
@@ -0,0 +1,264 @@
+package util
+
+import (
+	"errors"
+)
+
+var (
+	ErrPeriodInvalid    = errors.New("invalid period")
+	ErrPeriodStNegative = errors.New("start time is negative")
+	ErrPeriodIllegalArg = errors.New("illegal argument")
+)
+
+// Period a length or portion of time.
+// [StartTime, EndTime]
+// StartTime must be >= 0
+// EndTime negative values represent positive infinity
+type Period struct {
+	st int64
+	et int64
+}
+
+func (p Period) St() int64 {
+	return p.st
+}
+
+func (p Period) Et() int64 {
+	return p.et
+}
+
+// NewPeriod create a period
+func NewPeriod(st, et int64) (Period, error) {
+	if st < 0 {
+		return Period{}, ErrPeriodStNegative
+	}
+	if et >= 0 && st > et {
+		return Period{}, ErrPeriodInvalid
+	}
+	p := Period{
+		st: st,
+		et: et,
+	}
+	return p, nil
+}
+
+// NewPeriods create a set of period
+func NewPeriods(se ...int64) ([]Period, error) {
+	if len(se)%2 != 0 {
+		return nil, ErrPeriodIllegalArg
+	}
+	r := make([]Period, 0)
+	for i := 0; i < len(se); i = i + 2 {
+		p, err := NewPeriod(se[i], se[i+1])
+		if err != nil {
+			return nil, err
+		}
+		r = append(r, p)
+	}
+	return r, nil
+}
+
+// PeriodContains judge is t in period
+func PeriodContains(t int64, p Period) bool {
+	if t < 0 {
+		return false
+	}
+	if t >= p.st {
+		if p.et < 0 {
+			return true
+		}
+		if t <= p.et {
+			return true
+		}
+	}
+	return false
+}
+
+// PeriodUnion union two period
+func PeriodUnion(p1 Period, p2 Period) []Period {
+	switch {
+	case p1.et < 0 && p2.et < 0:
+		st := p2.st
+		et := p2.et
+		if p1.st < p2.st {
+			st = p1.st
+		}
+		ps, _ := NewPeriods(st, et)
+		return ps
+	case p1.et < 0:
+		switch {
+		case p2.et < p1.st:
+			return []Period{p1, p2}
+		case p2.et == p1.st:
+			st := p2.st
+			et := p1.et
+			ps, _ := NewPeriods(st, et)
+			return ps
+		case p2.et > p1.st:
+			st := p1.st
+			et := p1.et
+			if p2.st < p1.st {
+				st = p2.st
+			}
+			ps, _ := NewPeriods(st, et)
+			return ps
+		}
+	case p2.et < 0:
+		return PeriodUnion(p2, p1)
+	default:
+		switch {
+		case p2.et < p1.st:
+			return []Period{p1, p2}
+		case p2.et == p1.st:
+			st := p2.st
+			et := p1.et
+			ps, _ := NewPeriods(st, et)
+			return ps
+		case p2.et > p1.st && p2.et < p1.et, p2.et == p1.et:
+			st := p1.st
+			et := p1.et
+			if p2.st < p1.st {
+				st = p2.st
+			}
+			ps, _ := NewPeriods(st, et)
+			return ps
+		case p2.et > p1.et:
+			switch {
+			case p2.st < p1.st, p2.st == p1.st:
+				return []Period{p2}
+			case p2.st > p1.st && p2.st < p1.et, p2.st == p1.et:
+				st := p1.st
+				et := p2.et
+				ps, _ := NewPeriods(st, et)
+				return ps
+			case p2.st > p1.et:
+				return []Period{p1, p2}
+			}
+		}
+	}
+	return []Period{p1, p2}
+}
+
+// PeriodIntersect intersect two period
+func PeriodIntersect(p1 Period, p2 Period) []Period {
+	switch {
+	case p2.et < 0 && p1.et < 0:
+		st := p2.st
+		et := p2.et
+		if p2.st < p1.st {
+			st = p1.st
+		}
+		ps, _ := NewPeriods(st, et)
+		return ps
+	case p1.et < 0:
+		switch {
+		case p2.et < p1.st:
+			return []Period{}
+		case p2.et == p1.st:
+			st := p2.et
+			et := p2.et
+			ps, _ := NewPeriods(st, et)
+			return ps
+		case p2.et > p1.st:
+			st := p2.st
+			et := p2.et
+			if p2.st < p1.st {
+				st = p1.st
+			}
+			ps, _ := NewPeriods(st, et)
+			return ps
+		}
+	case p2.et < 0:
+		return PeriodIntersect(p2, p1)
+	default:
+		switch {
+		case p2.et < p1.st:
+			return []Period{}
+		case p2.et == p1.st:
+			st := p2.et
+			et := p2.et
+			ps, _ := NewPeriods(st, et)
+			return ps
+		case p2.et > p1.st && p2.et < p1.et, p2.et == p1.et:
+			st := p2.st
+			et := p2.et
+			if p2.st < p1.st {
+				st = p1.st
+			}
+			ps, _ := NewPeriods(st, et)
+			return ps
+		case p2.et > p1.et:
+			switch {
+			case p2.st < p1.st, p2.st == p1.st:
+				st := p1.st
+				et := p1.et
+				ps, _ := NewPeriods(st, et)
+				return ps
+			case p2.st > p1.st && p2.st < p1.et:
+				st := p2.st
+				et := p1.et
+				ps, _ := NewPeriods(st, et)
+				return ps
+			case p2.st == p1.et:
+				st := p2.st
+				et := p2.st
+				ps, _ := NewPeriods(st, et)
+				return ps
+			case p2.st > p1.et:
+				return []Period{}
+			}
+		}
+	}
+	return []Period{}
+}
+
+// PeriodsContains judge is t in periods
+func PeriodsContains(t int64, ps []Period) bool {
+	for _, p := range ps {
+		if PeriodContains(t, p) {
+			return true
+		}
+	}
+	return false
+}
+
+// AddPeriodToResultSet add a period to result set
+// ResultSet is a set of period, all elements has been union
+func AddPeriodToResultSet(p Period, rs []Period) []Period {
+	if len(rs) == 0 {
+		return []Period{p}
+	}
+	for i := 0; i < len(rs); i++ {
+		ps := PeriodUnion(p, rs[i])
+		if len(ps) == 1 {
+			t := rs[:i]
+			if i+1 < len(rs) {
+				t = append(t, rs[i+1:]...)
+			}
+			return AddPeriodToResultSet(ps[0], t)
+		}
+	}
+	return append(rs, p)
+}
+
+// PeriodsUnion union two periods
+func PeriodsUnion(a []Period, b []Period) []Period {
+	ps := append(a, b...)
+	rs := make([]Period, 0)
+	for _, p := range ps {
+		rs = AddPeriodToResultSet(p, rs)
+	}
+	return rs
+}
+
+// PeriodsIntersect intersect two periods
+func PeriodsIntersect(a []Period, b []Period) []Period {
+	c := make([]Period, 0)
+	for _, i := range a {
+		for _, j := range b {
+			k := PeriodIntersect(i, j)
+			c = append(c, k...)
+		}
+	}
+	return c
+}
---
+++ period_test.go
@@ -0,0 +1,981 @@
+package util
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewPeriod(t *testing.T) {
+	var newPeriod = func(st, et int64) Period {
+		p, _ := NewPeriod(st, et)
+		return p
+	}
+	type args struct {
+		st int64
+		et int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Period
+		wantErr error
+	}{
+		{
+			name: "point",
+			args: args{
+				st: 0,
+				et: 0,
+			},
+			want:    newPeriod(0, 0),
+			wantErr: nil,
+		},
+		{
+			name: "negative st",
+			args: args{
+				st: -1,
+				et: 0,
+			},
+			want:    newPeriod(0, 0),
+			wantErr: ErrPeriodStNegative,
+		},
+		{
+			name: "negative et",
+			args: args{
+				st: 0,
+				et: -1,
+			},
+			want:    newPeriod(0, -1),
+			wantErr: nil,
+		},
+		{
+			name: "st > et",
+			args: args{
+				st: 10,
+				et: 5,
+			},
+			want:    newPeriod(0, 0),
+			wantErr: ErrPeriodInvalid,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewPeriod(tt.args.st, tt.args.et)
+			if err != tt.wantErr {
+				t.Errorf("NewPeriod() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPeriod() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewPeriods(t *testing.T) {
+	var newPeriod = func(st, et int64) Period {
+		p, _ := NewPeriod(st, et)
+		return p
+	}
+	type args struct {
+		se []int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Period
+		wantErr error
+	}{
+		{
+			name: "empty period set",
+			args: args{
+				se: []int64{},
+			},
+			want:    []Period{},
+			wantErr: nil,
+		},
+		{
+			name: "normal periods",
+			args: args{
+				se: []int64{0, 1, 2, 3},
+			},
+			want:    []Period{newPeriod(0, 1), newPeriod(2, 3)},
+			wantErr: nil,
+		},
+		{
+			name: "illegal arg",
+			args: args{
+				se: []int64{0, 1, 2},
+			},
+			want:    nil,
+			wantErr: ErrPeriodIllegalArg,
+		},
+		{
+			name: "invalid period",
+			args: args{
+				se: []int64{0, 1, 2, 3, 4, -1, 5, 3},
+			},
+			want:    nil,
+			wantErr: ErrPeriodInvalid,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewPeriods(tt.args.se...)
+			if err != tt.wantErr {
+				t.Errorf("NewPeriods() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPeriods() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPeriodContains(t *testing.T) {
+	var newPeriod = func(st, et int64) Period {
+		p, _ := NewPeriod(st, et)
+		return p
+	}
+	type args struct {
+		t int64
+		p Period
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			args: args{
+				t: -1,
+				p: newPeriod(30, 40),
+			},
+			want: false,
+		},
+		{
+			args: args{
+				t: 0,
+				p: newPeriod(1, -1),
+			},
+			want: false,
+		},
+		{
+			args: args{
+				t: 35,
+				p: newPeriod(30, -1),
+			},
+			want: true,
+		},
+		{
+			args: args{
+				t: 35,
+				p: newPeriod(30, 40),
+			},
+			want: true,
+		},
+		{
+			args: args{
+				t: 35,
+				p: newPeriod(30, 32),
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PeriodContains(tt.args.t, tt.args.p); got != tt.want {
+				t.Errorf("PeriodContains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPeriodUnion(t *testing.T) {
+	var newPeriod = func(st, et int64) Period {
+		p, _ := NewPeriod(st, et)
+		return p
+	}
+	var newPeriods = func(se ...int64) []Period {
+		ps, _ := NewPeriods(se...)
+		return ps
+	}
+	type args struct {
+		p1 Period
+		p2 Period
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Period
+	}{
+		// p1 p2 all infinity
+		{
+			args: args{
+				p1: newPeriod(20, -1),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(20, -1),
+		},
+		{
+			args: args{
+				p1: newPeriod(10, -1),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(10, -1),
+		},
+		// p1 infinity
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(10, 20),
+			},
+			want: newPeriods(30, -1, 10, 20),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(20, 30),
+			},
+			want: newPeriods(20, -1),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(25, 35),
+			},
+			want: newPeriods(25, -1),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(10, 35),
+			},
+			want: newPeriods(10, -1),
+		},
+		// p2 infinity
+		{
+			args: args{
+				p1: newPeriod(10, 20),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(30, -1, 10, 20),
+		},
+		{
+			args: args{
+				p1: newPeriod(20, 30),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(20, -1),
+		},
+		{
+			args: args{
+				p1: newPeriod(25, 35),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(25, -1),
+		},
+		{
+			args: args{
+				p1: newPeriod(10, 35),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(10, -1),
+		},
+		// other
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(10, 20),
+			},
+			want: newPeriods(30, 40, 10, 20),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(20, 30),
+			},
+			want: newPeriods(20, 40),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(25, 35),
+			},
+			want: newPeriods(25, 40),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(35, 40),
+			},
+			want: newPeriods(30, 40),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(10, 60),
+			},
+			want: newPeriods(10, 60),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(30, 60),
+			},
+			want: newPeriods(30, 60),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(35, 60),
+			},
+			want: newPeriods(30, 60),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(40, 60),
+			},
+			want: newPeriods(30, 60),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(45, 60),
+			},
+			want: newPeriods(30, 40, 45, 60),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PeriodUnion(tt.args.p1, tt.args.p2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PeriodUnion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPeriodIntersect(t *testing.T) {
+	var newPeriod = func(st, et int64) Period {
+		p, _ := NewPeriod(st, et)
+		return p
+	}
+	var newPeriods = func(se ...int64) []Period {
+		ps, _ := NewPeriods(se...)
+		return ps
+	}
+	type args struct {
+		p1 Period
+		p2 Period
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Period
+	}{
+		// p1,p2 negative
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(10, -1),
+			},
+			want: newPeriods(30, -1),
+		},
+		{
+			args: args{
+				p1: newPeriod(10, -1),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(30, -1),
+		},
+		// p1 negative
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(10, 20),
+			},
+			want: newPeriods(),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(20, 30),
+			},
+			want: newPeriods(30, 30),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(25, 35),
+			},
+			want: newPeriods(30, 35),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(30, 35),
+			},
+			want: newPeriods(30, 35),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, -1),
+				p2: newPeriod(40, 50),
+			},
+			want: newPeriods(40, 50),
+		},
+		// p2 negative
+		{
+			args: args{
+				p1: newPeriod(10, 20),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(),
+		},
+		{
+			args: args{
+				p1: newPeriod(20, 30),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(30, 30),
+		},
+		{
+			args: args{
+				p1: newPeriod(25, 35),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(30, 35),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 35),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(30, 35),
+		},
+		{
+			args: args{
+				p1: newPeriod(40, 50),
+				p2: newPeriod(30, -1),
+			},
+			want: newPeriods(40, 50),
+		},
+		// other
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(10, 20),
+			},
+			want: newPeriods(),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(20, 30),
+			},
+			want: newPeriods(30, 30),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(25, 35),
+			},
+			want: newPeriods(30, 35),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(25, 40),
+			},
+			want: newPeriods(30, 40),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(25, 45),
+			},
+			want: newPeriods(30, 40),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(35, 45),
+			},
+			want: newPeriods(35, 40),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(40, 45),
+			},
+			want: newPeriods(40, 40),
+		},
+		{
+			args: args{
+				p1: newPeriod(30, 40),
+				p2: newPeriod(41, 45),
+			},
+			want: newPeriods(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PeriodIntersect(tt.args.p1, tt.args.p2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PeriodIntersect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPeriodsContains(t *testing.T) {
+	var newPeriods = func(se ...int64) []Period {
+		ps, _ := NewPeriods(se...)
+		return ps
+	}
+	type args struct {
+		t  int64
+		ps []Period
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			args: args{
+				t:  0,
+				ps: newPeriods(),
+			},
+			want: false,
+		},
+		{
+			args: args{
+				t:  30,
+				ps: newPeriods(0, -1),
+			},
+			want: true,
+		},
+		{
+			args: args{
+				t:  30,
+				ps: newPeriods(10, 20, 20, 30),
+			},
+			want: true,
+		},
+		{
+			args: args{
+				t:  30,
+				ps: newPeriods(0, 5, 5, 10, 10, 20, 25, 28),
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PeriodsContains(tt.args.t, tt.args.ps); got != tt.want {
+				t.Errorf("PeriodsContains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAddPeriodToResultSet(t *testing.T) {
+	var newPeriod = func(st, et int64) Period {
+		p, _ := NewPeriod(st, et)
+		return p
+	}
+	var newPeriods = func(se ...int64) []Period {
+		ps, _ := NewPeriods(se...)
+		return ps
+	}
+	type args struct {
+		p  Period
+		rs []Period
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Period
+	}{
+		// p -> rs => result
+		// [10, 20] -> [] => [10, 20]
+		// [10, -1] -> [] => [10, -1]
+		// [10, 20] -> [30, 40] => [30, 40] [10, 20]
+		// [20, 30] -> [30, 40] => [20, 40]
+		// [25, 35] -> [30, 40] => [25, 40]
+		// [40, 50] -> [30, 40] => [30, 50]
+		// [50, 60] -> [30, 40] =>  [30, 40] [50, 60]
+		// [10, -1] -> [30, 40] [50, 60] => [10, -1]
+		{
+			args: args{
+				p:  newPeriod(10, 20),
+				rs: newPeriods(),
+			},
+			want: newPeriods(10, 20),
+		},
+		{
+			args: args{
+				p:  newPeriod(10, -1),
+				rs: newPeriods(),
+			},
+			want: newPeriods(10, -1),
+		},
+		{
+			args: args{
+				p:  newPeriod(10, 20),
+				rs: newPeriods(30, 40),
+			},
+			want: newPeriods(30, 40, 10, 20),
+		},
+		{
+			args: args{
+				p:  newPeriod(20, 30),
+				rs: newPeriods(30, 40),
+			},
+			want: newPeriods(20, 40),
+		},
+		{
+			args: args{
+				p:  newPeriod(25, 35),
+				rs: newPeriods(30, 40),
+			},
+			want: newPeriods(25, 40),
+		},
+		{
+			args: args{
+				p:  newPeriod(40, 50),
+				rs: newPeriods(30, 40),
+			},
+			want: newPeriods(30, 50),
+		},
+		{
+			args: args{
+				p:  newPeriod(50, 60),
+				rs: newPeriods(30, 40),
+			},
+			want: newPeriods(30, 40, 50, 60),
+		},
+		{
+			args: args{
+				p:  newPeriod(10, -1),
+				rs: newPeriods(30, 40, 50, 60),
+			},
+			want: newPeriods(10, -1),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AddPeriodToResultSet(tt.args.p, tt.args.rs); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AddPeriodToResultSet() args %v, got %v, want %v", tt.args, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPeriodsUnion(t *testing.T) {
+	var newPeriods = func(se ...int64) []Period {
+		ps, _ := NewPeriods(se...)
+		return ps
+	}
+	type args struct {
+		a []Period
+		b []Period
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Period
+	}{
+		// normal
+		// [10, 20] [30, 40] => [10, 20] [30, 40]
+		// [20, 30] [30, 40] => [20, 40]
+		// [25, 35] [30, 40] => [25, 40]
+		// [30, 40] [30, 40] => [30, 40]
+		// [35, 45] [30, 40] => [30, 45]
+		// [40, 50] [30, 40] => [30, 50]
+		// [50, 60] [30, 40] => [50, 60] [30, 40]
+		{
+			args: args{
+				a: newPeriods(10, 20, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, 20, 30, 40),
+		},
+		{
+			args: args{
+				a: newPeriods(20, 30, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(20, 40),
+		},
+		{
+			args: args{
+				a: newPeriods(25, 35, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(25, 40),
+		},
+		{
+			args: args{
+				a: newPeriods(30, 40, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, 40),
+		},
+		{
+			args: args{
+				a: newPeriods(35, 45, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, 45),
+		},
+		{
+			args: args{
+				a: newPeriods(40, 50, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, 50),
+		},
+		{
+			args: args{
+				a: newPeriods(50, 60, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(50, 60, 30, 40),
+		},
+
+		// rigth positive infinity
+		// [10, 20] [30, -1] => [10, 20] [30, -1]
+		// [20, 30] [30, -1] => [20, -1]
+		// [25, 35] [30, -1] => [25, -1]
+		// [30, 40] [30, -1] => [30, -1]
+		// [35, 45] [30, -1] => [30, -1]
+		// [40, 50] [30, -1] => [30, -1]
+		// [50, 60] [30, -1] => [30, -1]
+		{
+			args: args{
+				a: newPeriods(10, 20, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, 20, 30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(20, 30, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(20, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(25, 35, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(25, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(30, 40, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(35, 45, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(40, 50, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(50, 60, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		// left positive infinity
+		// [10, -1] [30, 40] => [10, -1]
+		// [20, -1] [30, 40] => [20, -1]
+		// [25, -1] [30, 40] => [25, -1]
+		// [30, -1] [30, 40] => [30, -1]
+		// [35, -1] [30, 40] => [30, -1]
+		// [40, -1] [30, 40] => [30, -1]
+		// [50, -1] [30, 40] => [50, -1] [30, 40]
+		{
+			args: args{
+				a: newPeriods(10, -1, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(20, -1, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(20, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(25, -1, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(25, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(30, -1, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(35, -1, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(40, -1, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(50, -1, 30, 40),
+				b: newPeriods(),
+			},
+			want: newPeriods(50, -1, 30, 40),
+		},
+		// all infinity
+		// [10, -1] [30, -1] => [10, -1]
+		// [30, -1] [30, -1] => [30, -1]
+		// [40, -1] [30, -1] => [30, -1]
+		{
+			args: args{
+				a: newPeriods(10, -1, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(30, -1, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(40, -1, 30, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(30, -1),
+		},
+		// other
+		// [10, 20] [20, 30] [30, 40] [40, 50] [50, 60] => [10, 60]
+		// [10, -1] => [10, -1]
+		// [10, 20] [10, -1] [0, 5] => [10, -1] [0, 5]
+		// [10, 20] [30, 40] [50, 60] => [10, 20] [30, 40] [50, 60]
+		// [30, 40] [10, 60] => [10, 60]
+		{
+			args: args{
+				a: newPeriods(10, 20, 20, 30, 30, 40, 40, 50, 50, 60),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, 60),
+		},
+		{
+			args: args{
+				a: newPeriods(10, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(10, 20, 10, -1, 0, 5),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, -1, 0, 5),
+		},
+		{
+			args: args{
+				a: newPeriods(10, 20, 30, 40, 50, 60),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, 20, 30, 40, 50, 60),
+		},
+		{
+			args: args{
+				a: newPeriods(30, 40, 10, 60),
+				b: newPeriods(),
+			},
+			want: newPeriods(10, 60),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PeriodsUnion(tt.args.a, tt.args.b); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PeriodsUnion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPeriodsIntersect(t *testing.T) {
+	var newPeriods = func(se ...int64) []Period {
+		ps, _ := NewPeriods(se...)
+		return ps
+	}
+	type args struct {
+		a []Period
+		b []Period
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Period
+	}{
+		{
+			args: args{
+				a: newPeriods(0, 10, 30, -1),
+				b: newPeriods(5, 9, 20, -1),
+			},
+			want: newPeriods(5, 9, 30, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(30, 40, 50, -1),
+				b: newPeriods(10, 20, 30, 30, 32, 34, 38, 40, 60, -1),
+			},
+			want: newPeriods(30, 30, 32, 34, 38, 40, 60, -1),
+		},
+		{
+			args: args{
+				a: newPeriods(30, 40, 50, -1),
+				b: newPeriods(),
+			},
+			want: newPeriods(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PeriodsIntersect(tt.args.a, tt.args.b); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PeriodsIntersect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
